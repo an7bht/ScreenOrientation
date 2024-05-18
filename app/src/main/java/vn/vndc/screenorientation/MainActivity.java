1a9b180edd37f19b7d889cdda9b81c2c9b22318d
Truong An <andang.100202@gmail.com>
2024-05-18 10:35:47 +0700

    An

---
+++ app/src/main/java/vn/vndc/screenorientation/MainActivity.java
@@ -0,0 +1,40 @@
+package vn.vndc.screenorientation;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonPortrait = findViewById(R.id.buttonPortrait);
+        Button buttonLandscape = findViewById(R.id.buttonLandscape);
+
+        buttonPortrait.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SourceLockedOrientationActivity")
+            @Override
+            public void onClick(View v) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+        buttonLandscape.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+    }
+}
